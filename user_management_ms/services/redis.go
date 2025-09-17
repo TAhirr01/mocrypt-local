cbc562fb0c9f0eb5e0941ac074a17f3161b005bb
Tahir <tahirqasimov001@gmail.com>
2025-09-17 12:00:24 +0400

    new redis methods

--- user_management_ms/services/redis.go
+++ user_management_ms/services/redis.go
@@ -17,12 +17,12 @@ type IRedisService interface {
 	SetRefreshToken(userId uint, refreshToken string) error
 	GetRefreshToken(userId uint) (string, error)
 	DelRefreshToken(userId uint)
-	StoreSessionRedis(userID uint, sessionData *webauthn.SessionData) error
-	GetSessionRedis(userID uint) (*webauthn.SessionData, error)
-	DeleteSessionRedis(userID uint) error
-	StoreRegistrationSessionRedis(sessionId string, sessionData *webauthn.SessionData) error
-	GetRegistrationSessionRedis(sessionId string) (*webauthn.SessionData, error)
-	DeleteRegistrationSessionRedis(sessionId string) error
+	StoreSessionRedis(sessionId string, sessionData *webauthn.SessionData) error
+	GetSessionRedis(sessionId string) (*webauthn.SessionData, error)
+	DeleteSessionRedis(sessionId string) error
+	StoreRegistrationSessionRedis(userID uint, sessionData *webauthn.SessionData) error
+	GetRegistrationSessionRedis(userID uint) (*webauthn.SessionData, error)
+	DeleteRegistrationSessionRedis(userID uint) error
 }
 type RedisService struct {
 	rdb *redis.Client
@@ -44,14 +44,14 @@ func (s *RedisService) DelRefreshToken(userId uint) {
 	s.rdb.Del(ctx, fmt.Sprintf("refresh_%d", userId))
 }
 
-func (s *RedisService) StoreSessionRedis(userID uint, sessionData *webauthn.SessionData) error {
+func (s *RedisService) StoreSessionRedis(sessionId string, sessionData *webauthn.SessionData) error {
 	data, _ := json.Marshal(sessionData)
-	return s.rdb.Set(ctx, fmt.Sprintf("webauthn:%d", userID), data, 5*time.Minute).Err()
+	return s.rdb.Set(ctx, fmt.Sprintf("webauthn:%s", sessionId), data, 5*time.Minute).Err()
 }
 
 // get session
-func (s *RedisService) GetSessionRedis(userID uint) (*webauthn.SessionData, error) {
-	val, err := s.rdb.Get(ctx, fmt.Sprintf("webauthn:%d", userID)).Result()
+func (s *RedisService) GetSessionRedis(sessionId string) (*webauthn.SessionData, error) {
+	val, err := s.rdb.Get(ctx, fmt.Sprintf("webauthn:%s", sessionId)).Result()
 	if err != nil {
 		return nil, err
 	}
@@ -64,16 +64,16 @@ func (s *RedisService) GetSessionRedis(userID uint) (*webauthn.SessionData, erro
 }
 
 // delete session
-func (s *RedisService) DeleteSessionRedis(userID uint) error {
-	return s.rdb.Del(ctx, fmt.Sprintf("webauthn:%d", userID)).Err()
+func (s *RedisService) DeleteSessionRedis(sessionId string) error {
+	return s.rdb.Del(ctx, fmt.Sprintf("webauthn:%s", sessionId)).Err()
 }
 
-func (s *RedisService) StoreRegistrationSessionRedis(sessionId string, sessionData *webauthn.SessionData) error {
+func (s *RedisService) StoreRegistrationSessionRedis(userId uint, sessionData *webauthn.SessionData) error {
 	data, _ := json.Marshal(sessionData)
-	return s.rdb.Set(ctx, fmt.Sprintf("webauthn:%s", sessionId), data, 5*time.Minute).Err()
+	return s.rdb.Set(ctx, fmt.Sprintf("webauthn:%d", userId), data, 5*time.Minute).Err()
 }
-func (s *RedisService) GetRegistrationSessionRedis(sessionId string) (*webauthn.SessionData, error) {
-	val, err := s.rdb.Get(ctx, fmt.Sprintf("webauthn:%s", sessionId)).Result()
+func (s *RedisService) GetRegistrationSessionRedis(userId uint) (*webauthn.SessionData, error) {
+	val, err := s.rdb.Get(ctx, fmt.Sprintf("webauthn:%d", userId)).Result()
 	if err != nil {
 		return nil, err
 	}
@@ -84,6 +84,6 @@ func (s *RedisService) GetRegistrationSessionRedis(sessionId string) (*webauthn.
 	}
 	return sessionData, nil
 }
-func (s *RedisService) DeleteRegistrationSessionRedis(sessionId string) error {
-	return s.rdb.Del(ctx, fmt.Sprintf("webauthn:%d", sessionId)).Err()
+func (s *RedisService) DeleteRegistrationSessionRedis(userId uint) error {
+	return s.rdb.Del(ctx, fmt.Sprintf("webauthn:%d", userId)).Err()
 }
