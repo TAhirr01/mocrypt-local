96ac7bc99d0f55be532b94d072227623749ab2f9
tahir <tahirqasimov001@gmail.com>
2025-09-30 11:23:10 +0400

    deleted some response fields

--- user_management_ms/dtos/response/otp_response.go
+++ user_management_ms/dtos/response/otp_response.go
@@ -13,12 +13,10 @@ const (
 )
 
 type OTPResponse struct {
-	UserId        uint   `json:"user_id"`
-	Email         string `json:"email"`
-	Phone         string `json:"phone"`
-	EmailVerified bool   `json:"email_verified"`
-	PhoneVerified bool   `json:"phone_verified"`
-	Status        string `json:"status"`
+	UserId uint   `json:"user_id"`
+	Email  string `json:"email"`
+	Phone  string `json:"phone"`
+	Status string `json:"status"`
 }
 
 type SendOTPResponse struct {
@@ -28,29 +26,25 @@ type SendOTPResponse struct {
 }
 
 type RegisterResponse struct {
-	UserId        uint   `json:"user_id"`
-	UserType      string `json:"user_type"`
-	Email         string `json:"email"`
-	Phone         string `json:"phone"`
-	EmailVerified bool   `json:"email_verified"`
-	PhoneVerified bool   `json:"phone_verified"`
-	Completed     bool   `json:"completed"`
-	Status        string `json:"status"`
-	HasPin        bool   `json:"has_pin"`
+	UserId   uint   `json:"user_id"`
+	UserType string `json:"user_type"`
+	Email    string `json:"email"`
+	Phone    string `json:"phone"`
+	Status   string `json:"status"`
 }
 
 type OTPResponsePhone struct {
+	UserId        uint   `json:"user_id"`
 	Phone         string `json:"phone"`
 	PhoneVerified bool   `json:"phone_verified"`
 	Status        string `json:"status"`
-	Message       string `json:"message"`
 }
 
 type OTPResponseEmail struct {
+	UserId        uint   `json:"user_id"`
 	Email         string `json:"email"`
 	EmailVerified bool   `json:"email_verified"`
 	Status        string `json:"status"`
-	Message       string `json:"message"`
 }
 
 type GoogleResponse struct {
