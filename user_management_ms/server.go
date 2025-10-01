f35fe3e354c42a4b019e11f5da2a9a8b4cbd0cf2
tahir <tahirqasimov001@gmail.com>
2025-10-01 19:10:25 +0400

    added validation middleware to endpoints

--- user_management_ms/server.go
+++ user_management_ms/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"user_management_ms/config"
 	"user_management_ms/controller"
+	"user_management_ms/dtos/request"
 	"user_management_ms/middleware"
 
 	"github.com/gofiber/fiber/v2"
@@ -49,27 +50,27 @@ func (s *Server) Start() *fiber.App {
 
 	authGroup := apiVersion.Group("/auth")
 	authGroup.Use(middleware.LoggingMiddleware(s.Logger))
-	authGroup.Post("/request-otp", s.AuthController.RegisterRequestOTP)
-	authGroup.Post("/verify-otp/:userId", s.AuthController.VerifyRegisterOTP)
-	authGroup.Post("/resend-otp", s.AuthController.ResendOTP)
-	authGroup.Post("/complete-registration/:userId", s.AuthController.CompleteRegistration)
-	authGroup.Post("/login", s.AuthController.LoginLocal)
-	authGroup.Post("/verify-login-otp/:userId", s.AuthController.VerifyLoginOTP)
-	authGroup.Post("/refresh-token", s.AuthController.RefreshToken)
+	authGroup.Post("/request-otp", middleware.ValidateBody[request.StartRegistration](), s.AuthController.RegisterRequestOTP)
+	authGroup.Post("/verify-otp/:userId", middleware.ValidateBody[request.EmailAndPhoneOTP](), s.AuthController.VerifyRegisterOTP)
+	authGroup.Post("/resend-otp", middleware.ValidateBody[request.OTPRequest](), s.AuthController.ResendOTP)
+	authGroup.Post("/complete-registration/:userId", middleware.ValidateBody[request.BirthDateAndPassword](), s.AuthController.CompleteRegistration)
+	authGroup.Post("/login", middleware.ValidateBody[request.LoginLocalRequest](), s.AuthController.LoginLocal)
+	authGroup.Post("/verify-login-otp/:userId", middleware.ValidateBody[request.EmailAndPhoneOTP](), s.AuthController.VerifyLoginOTP)
+	authGroup.Post("/refresh-token", middleware.ValidateBody[request.RefreshTokenReq](), s.AuthController.RefreshToken)
 	authGroup.Get("/setup-2fa", middleware.AuthMiddleware(), s.AuthController.Setup2FA)
-	authGroup.Post("/verify-2fa", middleware.AuthMiddleware(), s.AuthController.Verify2FA)
-	authGroup.Post("/pin/set/:userId", s.AuthController.SetPIN)
-	authGroup.Post("/pin/verify/:userId", s.AuthController.VerifyPIN)
+	authGroup.Post("/verify-2fa", middleware.ValidateBody[request.VerifyTwoFa](), middleware.AuthMiddleware(), s.AuthController.Verify2FA)
+	authGroup.Post("/pin/set/:userId", middleware.ValidateBody[request.PinReq](), s.AuthController.SetPIN)
+	authGroup.Post("/pin/verify/:userId", middleware.ValidateBody[request.PinReq](), s.AuthController.VerifyPIN)
 	authGroup.Post("/qr", s.AuthController.QrLoginRequest)
 	authGroup.Post("/qr/approve", middleware.AuthMiddleware(), s.AuthController.ApproveLoginRequest)
 	authGroup.Post("/qr/:sessionId/status", s.AuthController.CheckLoginRequest)
 
 	authGroup.Get("/google/call-back", s.GoogleAuthController.GoogleCallback)
 	authGroup.Get("/google/login", s.GoogleAuthController.GoogleLogin)
-	authGroup.Post("/google/request-otp/:userId", s.GoogleAuthController.GoogleRequestPhoneOTP)
-	authGroup.Post("/google/verify-otp/:userId", s.GoogleAuthController.GoogleVerifyRequestOTP)
-	authGroup.Post("/google/complete-registration/:userId", s.GoogleAuthController.CompleteGoogleRegistration)
-	authGroup.Post("/google/login/verify-otp/:userId", s.GoogleAuthController.GoogleVerifyLoginRequestOtp)
+	authGroup.Post("/google/request-otp/:userId", middleware.ValidateBody[request.PhoneRequest](), s.GoogleAuthController.GoogleRequestPhoneOTP)
+	authGroup.Post("/google/verify-otp/:userId", middleware.ValidateBody[request.PhoneOTP](), s.GoogleAuthController.GoogleVerifyRequestOTP)
+	authGroup.Post("/google/complete-registration/:userId", middleware.ValidateBody[request.BirthDateAndPassword](), s.GoogleAuthController.CompleteGoogleRegistration)
+	authGroup.Post("/google/login/verify-otp/:userId", middleware.ValidateBody[request.EmailOTP](), s.GoogleAuthController.GoogleVerifyLoginRequestOtp)
 	authGroup.Post("/google/login/resend-otp/:userId", s.GoogleAuthController.ResendOTP)
 
 	authGroup.Post("/register/start", middleware.AuthMiddleware(), s.WebAuthnController.RegisterStart)
